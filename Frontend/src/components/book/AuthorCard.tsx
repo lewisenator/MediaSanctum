611d26a527628b183278ee514e4bf87e190711a2
Kenneth Lewis <lewisenator@gmail.com>
2026-07-23 11:31:50 -0700

    Frontend: implement authors index page

---
+++ Frontend/src/components/book/AuthorCard.tsx
@@ -0,0 +1,58 @@
+import type { Author } from '#/client/authorClient.ts';
+import { Link } from '@tanstack/react-router';
+import { RxPerson } from "react-icons/rx";
+
+type AuthorCardProps = {
+  author: Author;
+};
+
+const AuthorCard = (
+  {author}: AuthorCardProps
+) => {
+  return (
+    <Link to="/authors/$authorId" params={{ authorId: author.id }} className="library-card group flex flex-col" title={author.name}>
+      <div className="library-card-cover min-h-2/4">
+        {author.image ? (
+          <img
+            src={author.image.url}
+            alt={`${author.name} Image`}
+            className="cover-image w-full"
+          />
+        ) : (
+          <div className="cover-image flex items-center justify-center h-40 min-h-40 max-w-30 text-center text-4xl shadow-none!">
+            <RxPerson />
+          </div>
+        )}
+      </div>
+      <div className="library-card-meta ">
+        <div>
+          <p className="library-card-title font-display font-medium text-md text-wrap tracking-tight overflow-hidden" title={author.name}>
+            {author.name}
+          </p>
+          <div className="text-xs text-textDim mt-1 overflow-hidden">
+            {author.name}
+          </div>
+        </div>
+        <div className="flex flex-row justify-between mt-2">
+          <span className="tabular-nums font-mono text-textMute text-xs">
+            {author.bornYear && (
+              <span className="flex flex-row gap-1">
+                <span className="font-mono text-accent">{author.bornYear}</span>
+                <span>-</span>
+                <span className="font-mono text-accent">
+                  {author.deathYear ? (
+                    author.deathYear
+                  ) : (
+                    "Present"
+                  )}
+                </span>
+              </span>
+            )}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default AuthorCard;
--- Frontend/src/routes/(authenticated)/_sidebar/authors/index.tsx
+++ Frontend/src/routes/(authenticated)/_sidebar/authors/index.tsx
@@ -4,6 +4,8 @@ import { getAuthors } from '#/client/authorClient.ts';
 import { useState } from 'react';
 import { IoIosSearch } from 'react-icons/io';
 import Main from '#/components/layout/Main.tsx';
+import { RxPerson } from "react-icons/rx";
+import AuthorCard from '#/components/book/AuthorCard.tsx';
 
 const authorsQueryOptions = () => queryOptions({
   queryKey: ['authors'],
@@ -23,56 +25,59 @@ function AuthorsPage() {
 
   return (
     <Main>
-      <div className="w-full flex flex-row flex-wrap justify-between">
-        <h1 className="font-display font-semibold text-3xl text-text">
-          Authors
-          { authors && (
-            <span className="text-xs text-textMute ml-2">
-              {authors.length > 1 ? `${authors.length} authors` : '1 author'}
-            </span>
-          )}
-        </h1>
-        <Link
-          to="/authors/search"
-          className="btn btn-secondary text-sm"
-        >
-          Add New Author
-        </Link>
-      </div>
+      <div className="flex flex-col flex-1 overflow-y-hidden max-w-6xl mx-auto">
+        <div className="w-full flex flex-row flex-wrap justify-between">
+          <h1 className="font-display font-semibold text-3xl text-text">
+            Authors
+            { authors && (
+              <span className="text-xs text-textMute ml-2">
+                {authors.length > 1 ? `${authors.length} authors` : '1 author'}
+              </span>
+            )}
+          </h1>
+          <Link
+            to="/authors/search"
+            className="btn btn-secondary text-sm"
+          >
+            Add New Author
+          </Link>
+        </div>
 
-      <div className="flex flex-wrap items-center gap-2 mt-6">
-        <div className="card p-4 w-full">
-          <label className="card-label">
-            Search your followed authors
-          </label>
-          <div className="search-bar relative flex-1 min-w-[16rem] w-full">
-            <IoIosSearch />
-            <input
-              type="search"
-              placeholder="First Name, Last Name, etc..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            />
+        <div className="w-full flex flex-wrap items-center gap-2 mt-6">
+          <div className="card p-4 w-full">
+            <label className="card-label">
+              Search your catalogue
+            </label>
+            <div className="search-bar relative flex-1 min-w-[16rem] w-full">
+              <IoIosSearch />
+              <input
+                type="search"
+                placeholder="First Name, Last Name, etc..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      { authors && authors.length > 0 ? (
-        <div className="mt-6">
-          { authors.map((author) => (
-            <div key={author.id}>
-              <div>{author.name}</div>
-              {author.image && (
-                <img src={author.image?.url} alt={`${author.name} Mugshot`} className="object-cover h-40 max-w-30" />
-              )}
-
-              <div>{JSON.stringify(author)}</div>
+        { authors && authors.length > 0 ? (
+          <div className="mt-6 flex flex-wrap items-center overflow-y-scroll w-full">
+            <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 overflow-y-scroll">
+              { authors.map((author) => (
+                <AuthorCard author={author} key={author.id} />
+              ))}
             </div>
-          ))}
-        </div>
-      ) : (
-        <div>No authors found.</div>
-      )}
+          </div>
+        ) : (
+          <div className="mt-6 card p-6 w-full flex flex-col justify-center items-center gap-2 flex-1">
+            <div className="text-4xl">
+              <RxPerson />
+            </div>
+            <div className="text-lg font-display text-text">No books yet.</div>
+            <div className="text-sm font-display font-medium text-textDim">Add a book to your catalogue by clicking the "Add New Book" at button.</div>
+          </div>
+        )}
+      </div>
     </Main>
   )
 }
